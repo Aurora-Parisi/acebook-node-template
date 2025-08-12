bc3a97419257a44c1321cee48e270533b039724b
Aurora <aurora.parisi98@outlook.com>
2025-08-12 13:12:43 +0100

    modified dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-# Example: Install Node.js dependencies
+# Create the directory if it doesn't exist
+mkdir -p /var/www/html
+# Change into the directory and install dependencies
 cd /var/www/html
 npm install
