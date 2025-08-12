eebf5d3c0f12d9631c4959cff83ece4d2a82d214
Aurora <aurora.parisi98@outlook.com>
2025-08-12 13:18:13 +0100

    added create_directore.sh and updated appspec and dependencies

---
+++ scripts/create_directory.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Create the directory if it doesn't exist
+mkdir -p /var/www/html
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Create the directory if it doesn't exist
-mkdir -p /var/www/html
-# Change into the directory and install dependencies
+# Change into the application directory and install dependencies
 cd /var/www/html
 npm install
